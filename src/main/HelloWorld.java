b350949e562456e7b8521c0aea432cfc40c6efab
Lyra <lauriemason@gmail.com>
2021-02-17 19:28:45 +0100

    Clase 17/02

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -8,52 +8,51 @@ import java.util.Locale;
 public class HelloWorld {
     // Esto es un comentario
     /* Main es el método de entrada del programa
-    * La primera linea ejecutable va a ser la del método Main
-    */
+     * La primera linea ejecutable va a ser la del método Main
+     */
     public static void main(String[] args) throws IOException {
         HelloWorld.menu();
         // Siguiente clase
-        // Hacer que el programa te diga que has escrito una opción incorrecta
         // Potencias
         // Condiciones con or y and
-
-
     }
-    public static void edad(String nombre) throws IOException {
+
+    public static void edad(String nombre) {
         // Esta linea de código sirve para escribir por consola
         System.out.println(nombre + " ¿Qué edad tienes?");
         // Estas dos variables sirven para leer por consola
         InputStreamReader streamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(streamReader);
-        Boolean edadincorrecta = false;
-        String edad ="";
-        Integer edad1 =1;
-        while(edadincorrecta == false){
-            try{ // Esta es la linea que lee por consola
+        boolean edadincorrecta = false;
+        String edad = "";
+        int edad1 = 1;
+        while (!edadincorrecta) {
+            try { // Esta es la linea que lee por consola
                 edad = bufferedReader.readLine();
                 edad1 = Integer.parseInt(edad);
                 edadincorrecta = true;
-            } catch (Exception ex){
+            } catch (Exception ex) {
                 System.out.println("Edad incorrecta. Por favor, escribe un número.");
             }
 
         }
         System.out.println("Tienes " + edad + " años :D");
-        if (edad1<18){
+        if (edad1 < 18) {
             System.out.println("Eres menor de edad D:");
         } else {
             System.out.println("Eres mayor de edad");
         }
-        while(edad1>0){
-            System.out.println("Cuenta atrás"+ edad1);
-            edad1= edad1 - 1;
+        while (edad1 > 0) {
+            System.out.println("Cuenta atrás" + edad1);
+            edad1 = edad1 - 1;
         }
         //Como edad la hemos definido de tipo String pero ahora queremos hacer una operación numérica
         // tenemos que convertirla al tipo Integer usando Integer.parseInt(variable)
-        Integer segundaedad = Integer.parseInt(edad) + 1;
+        int segundaedad = Integer.parseInt(edad) + 1;
         System.out.println("Y el año que viene tendrás " + segundaedad);
 
     }
+
     public static String saludo() throws IOException {
         System.out.println("Hola, ¿Cómo te llamas?");
         InputStreamReader streamReader = new InputStreamReader(System.in);
@@ -62,7 +61,8 @@ public class HelloWorld {
         System.out.println("Hola " + nombre + " :D");
         return nombre;
     }
-    public static void primerasesion(){
+
+    public static void primerasesion() {
         System.out.println("HolaMundo");
         String mensaje = "Hola Lala! =)";
         System.out.println(mensaje);
@@ -71,10 +71,10 @@ public class HelloWorld {
 
         String color;
         color = "negro";
-        System.out.println("Me gusta el color " +     color);
+        System.out.println("Me gusta el color " + color);
     }
 
-    public static void menu () throws IOException {
+    public static void menu() throws IOException {
         System.out.println("Elige una de las opciones");
         System.out.println("a) Introducir nombre");
         System.out.println("b) Introducir edad");
@@ -83,24 +83,46 @@ public class HelloWorld {
         String opcion;
         InputStreamReader streamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(streamReader);
-        opcion= bufferedReader.readLine();
-        String nombre="Desconocido";
-        while (!opcion.equals("x")){
-            if(opcion.equals("a") || opcion.equals("A")){
-                nombre= HelloWorld.saludo();
+        opcion = bufferedReader.readLine();
+        String nombre = "Desconocide";
+        while (!opcion.equals("x")) {
+
+            if (opcion.equals("a") || opcion.equals("A")) {
+                nombre = HelloWorld.saludo();
+            } else {
+                if (opcion.equalsIgnoreCase("b")) {
+                    HelloWorld.edad(nombre);
+                } else {
+                    if (opcion.equals("c")) {
+                        HelloWorld.primerasesion();
+                    } else {
+                        System.out.println( "Opción incorrecta, elige otra porfavor" );
+                    }
+                }
             }
-            if(opcion.toLowerCase().equals("b")){
-                HelloWorld.edad(nombre);
-            }
-            if (opcion.equals("c")){
-                HelloWorld.primerasesion();
-            }
+            /*
+           switch(opcion){
+               case "A":
+               case "a":
+                   nombre=HelloWorld.saludo();
+                   break;
+               case "b":
+                   HelloWorld.edad(nombre);
+                   break;
+               case"c":
+                   HelloWorld.primerasesion();
+                   break;
+               default:
+                   System.out.println("Opción incorrecta, tonto. ");
+                   break;
+           } */
+
             System.out.println("Elige una de las opciones");
             System.out.println("a) Introducir nombre");
             System.out.println("b) Introducir edad");
             System.out.println("c) Reproducir primera sesión");
             System.out.println("x) Salir");
-            opcion= bufferedReader.readLine();
+            opcion = bufferedReader.readLine();
         }
     }
 }
--- src/main/Practica1.java
+++ src/main/Practica1.java
@@ -7,17 +7,17 @@ import java.io.InputStreamReader;
 public class Practica1 {
 
     public static void main(String[] args) throws IOException {
-        Practica1.presentación();
+        Practica1.presentacion();
         Practica1.pregunta();
     }
 
-    public static void presentación(){
+    public static void presentacion() {
         String mensaje1 = "Soy un fantasma...";
         System.out.println(mensaje1);
         System.out.println(".....");
     }
 
-    public static String pregunta()  throws IOException{
+    public static String pregunta() throws IOException {
         System.out.println("Y tú, ¿Cómo te llamas?");
         InputStreamReader streamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(streamReader);
@@ -26,7 +26,7 @@ public class Practica1 {
         return nombre;
     }
 
-    public static void hermanos(){
-            System.out.println();
+    public static void hermanos() {
+        System.out.println();
     }
 }
